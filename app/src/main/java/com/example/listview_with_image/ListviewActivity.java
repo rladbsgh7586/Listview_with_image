eb7f925ed52288e751121a8c7561df9dd4223d07
songmho <songmho@gmail.com>
2015-01-08 23:17:17 +0900

    일부 코드 수정

--- app/src/main/java/com/example/listview_with_image/ListviewActivity.java
+++ app/src/main/java/com/example/listview_with_image/ListviewActivity.java
@@ -1,11 +1,11 @@
 package com.example.listview_with_image;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.widget.ListView;
 import java.util.ArrayList;
 
-public class ListviewActivity extends Activity {
+public class ListviewActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +14,7 @@ public class ListviewActivity extends Activity {
 
         ListView listView=(ListView)findViewById(R.id.listview);
 
-        ArrayList<Listviewitem> data=new ArrayList<Listviewitem>();
+        ArrayList<Listviewitem> data=new ArrayList<>();
         Listviewitem lion=new Listviewitem(R.drawable.lion,"Lion");
         Listviewitem tiger=new Listviewitem(R.drawable.tiger,"Tiger");
         Listviewitem dog=new Listviewitem(R.drawable.dog,"Dog");
@@ -28,5 +28,4 @@ public class ListviewActivity extends Activity {
         ListviewAdapter adapter=new ListviewAdapter(this,R.layout.listviewitem,data);
         listView.setAdapter(adapter);
     }
-
 }
